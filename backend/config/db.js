624e6f11ff8c006e9ff3836c0addd4ada8e4db17
Budi Setiawan <muhammadbudisetiawan@mail.ugm.ac.id>
2024-11-05 19:55:55 +0700

    Fix : [BE] database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (error) {
-        console.error(error.message);
+        console.error('MongoDB connection error:', error.message);
         process.exit(1);
     }
+    console.log('MongoDB URI:', process.env.MONGO_URI);
 };
+
 module.exports = connectDB;
