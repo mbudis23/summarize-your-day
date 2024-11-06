840ad3cb2294020ebf359bd71c1174b9c262fd57
Budi Setiawan <muhammadbudisetiawan@mail.ugm.ac.id>
2024-11-06 09:06:42 +0700

    Feat: [BE] Logging system

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const logRequest = require('./utils/logger');
 
 userRoute = require('./routes/userRoute');
 
 dotenv.config();
 
-const app = express();
 connectDB();
-
+const app = express();
 app.use(express.json());
 
 app.use('/api/users', userRoute)
+app.use(express.json());
+app.use(logRequest);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
